606c670c444abd40f0f1fb6b99c4e022d46f789c
cipta <kadekcipta@gmail.com>
2015-08-06 11:08:57 +0700

    Update the sample

--- slackconnect/examples/logger/main.go
+++ slackconnect/examples/logger/main.go
@@ -5,19 +5,12 @@ import (
 	"goutils/slackconnect"
 )
 
-const webhookUri = "https://hooks.slack.com/services/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"
+const webhookUri = "<your webhook uri>"
 
 func main() {
-	logger := slackconnect.NewLogger(webhookUri, "slack.db", "#systemd", "Magicsoft-Asia Systems", nil)
+	logger := slackconnect.NewLogger(webhookUri, "slack.db", "#orders", "The GOLEK", nil)
 	logger.Open()
 	defer logger.Close()
-
-	logger.Info("testing")
-	//	for i := 0; i < 2; i++ {
-	//		go func(n int) {
-	//			logger.Info(fmt.Sprintf("Info #%d <https://magicsoft-asia.beanstalkapp.com/goutils/browse/git/bucket/bolt_bucket.go?ref=c-92494c06808d6a1389732ad7934e111ba6d61105|Some Link>", n))
-	//		}(i + 1)
-	//	}
-
+	logger.Info("Hello..")
 	fmt.Scanln()
 }
